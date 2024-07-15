8d69e2a7e9cf11a2d610b206376bc75e851f9357
swagatpadhi <swagatpadhi03@gmail.com>
2024-07-15 18:25:59 +0530

    First commit in this repo.

---
+++ src/ReverseNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter number: ");
+        int num = scn.nextInt();
+
+        while(num > 0){
+            int ext = num % 10;
+            System.out.print(ext);
+            num/=10;
+        }
+    }
+}
