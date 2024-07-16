58c1538ce6121661f049b618238601751f936a9c
swagatpadhi <swagatpadhi03@gmail.com>
2024-07-16 13:58:41 +0530

    Added code to check whether a number is prime/composite.

---
+++ src/PrimeOrNot.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int num = scn.nextInt();
+
+        if(num == 2){
+            System.out.println("prime");
+        }else {
+            boolean flag = true;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == false) {
+                System.out.println("Composite");
+            } else {
+                System.out.println("Prime");
+            }
+        }
+    }
+}
