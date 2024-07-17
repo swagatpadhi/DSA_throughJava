dad51714329fadf073f5642bfcd0305ef10be86d
swagatpadhi <swagatpadhi03@gmail.com>
2024-07-17 20:35:44 +0530

    added another pattern problem.

---
+++ src/Patterns/CharacterPattern2.java
@@ -0,0 +1,18 @@
+package Patterns;
+//A
+//AB
+//ABC
+//ABCD
+public class CharacterPattern2 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 4; i++) {
+            int x = 65;
+            for(int j = 1; j <= i; j++) {
+                char ch = (char) x;
+                System.out.print(ch);
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
