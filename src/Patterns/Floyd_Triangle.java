958822f5b0631b822832461ab7a118b83425ac8c
swagatpadhi <swagatpadhi03@gmail.com>
2024-07-18 01:28:55 +0530

    Added Floyd's Triangle code.

---
+++ src/Patterns/Floyd_Triangle.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Floyd_Triangle {
+    public static void main(String[] args) {
+        int x = 1;
+        for(int i = 1; i <= 4; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(x + " ");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
