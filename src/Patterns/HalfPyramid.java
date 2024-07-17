54be7f22c8172a959b42c9be9a641096356e28b3
swagatpadhi <swagatpadhi03@gmail.com>
2024-07-17 16:44:07 +0530

    modified HalfPyramid pattern a bit.

--- src/Patterns/HalfPyramid.java
+++ src/Patterns/HalfPyramid.java
@@ -3,10 +3,8 @@ package Patterns;
 public class HalfPyramid {
     public static void main(String[] args) {
         for(int i = 1; i <= 4; i++) {
-            int count = 1;
             for(int j = 1; j <= i; j++) {
-                System.out.print(count);
-                count++;
+                System.out.print(j);
             }
             System.out.println();
         }
