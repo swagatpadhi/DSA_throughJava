f493ce02847e5bbca1e3a68d4aaa28678a8d8fa3
swagatpadhi <swagatpadhi03@gmail.com>
2024-07-19 23:20:41 +0530

    added a program to find sum of 2 numbers with the help of a method.

---
+++ src/Function.java
@@ -0,0 +1,10 @@
+public class Function {
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println(result);
+    }
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
