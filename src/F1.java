a558a86cebadd5ccda9901add4bd98219708eb93
swagatpadhi <swagatpadhi03@gmail.com>
2024-07-21 13:51:55 +0530

    added a normal programming for function.

---
+++ src/F1.java
@@ -0,0 +1,12 @@
+public class F1 {
+    public static int printHello(){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        return 3;
+    }
+    public static void main(String[] args) {
+        int res = printHello();
+        System.out.println(res);
+    }
+}
